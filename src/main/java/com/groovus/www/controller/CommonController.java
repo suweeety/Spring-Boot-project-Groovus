cce2b4938d852bd4489516d387917a32143a97a0
10-5\kkw <jeongeun587@gmail.com>
2024-04-22 15:27:25 +0900

    2024-04-22 (오후) 기능별 메뉴 색상변경 , 기능별 html(예시)구현

--- src/main/java/com/groovus/www/controller/CommonController.java
+++ src/main/java/com/groovus/www/controller/CommonController.java
@@ -17,7 +17,44 @@ public class CommonController {
         return "/main/test";
     }
 
+    @GetMapping("/task/taskList")
+    public void goTaskList(){
+        //업무리스트로 이동
 
+    }
 
+    @GetMapping("/calendar/schedule")
+    public String goScheduleManagement(){
+        //일정관리로 이동
+
+        return "/calendar/schedule";
+    }
+
+    @GetMapping("/message/messageList")
+    public void goMessageList(){
+        //쪽지함 이동
+
+    }
+
+    @GetMapping("/drive/drive")
+    public void goDrive(){
+        //드라이브 이동
+
+    }
+    @GetMapping("/drive/bin")
+    public void goDriveBin(){
+        //드라이브 휴지통
+
+    }
+    @GetMapping("/conference/webConference")
+    public void goConference(){
+        //화상회의로 이동
+
+    }
+    
+    @GetMapping("/setting")
+    public void goSetting(){
+        //설정페이지로 이동
+    }
 
 }
